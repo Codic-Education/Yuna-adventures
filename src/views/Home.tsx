3ac471eb40d8b6cdee095eee2c360893471f4926
Mohammad <kurtular.vadisi@gmail.com>
2021-02-24 00:43:32 +0100

    Create clouds component.

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Clouds from '../components/Clouds';
 import Ground from '../components/Ground';
 import ViewBackground from '../components/ViewBackground';
 
 const Home = () => {
 	return (
 		<ViewBackground>
+			<Clouds />
 			<Ground variant="farm" />
 		</ViewBackground>
 	);
 };
 
 export default Home;
+
