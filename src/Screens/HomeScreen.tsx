4e971a83cef545a49c1bd61de9cf5b066dff6081
khazar <khAZAR.MEHRABAN@GMAIL.COM>
2021-02-25 19:49:31 +0100

    refact

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,12 +1,10 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Dimensions, Text } from 'react-native';
 import Clouds from '../components/Clouds';
 import Ground from '../components/Ground';
 import Button from '../components/inputs/Button';
 import ViewBackground from '../components/ViewBackground';
 import { createStyle } from '../providers/Theme';
-const { width, height } = Dimensions.get('screen');
 
 const HomeScreen = () => {
 	const navigation = useNavigation();
@@ -28,12 +26,12 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
-const useStyles = createStyle({
+const useStyles = createStyle(({ dimensions: { screenWidth } }) => ({
 	button: {
 		borderColor: '#000',
 		borderWidth: 2,
 		top: 10,
-		width: width / 3,
+		width: screenWidth / 3,
 		height: 'auto',
 	},
-});
+}));
