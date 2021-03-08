2de5a9224c459315319a14650a96ac73070f6fe2
khazar <khAZAR.MEHRABAN@GMAIL.COM>
2021-03-08 17:09:35 +0100

    unexpeted buggfix lang

--- src/components/TranslatedText.tsx
+++ src/components/TranslatedText.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { t } from 'i18n-js';
 import { Text, TextProps } from 'react-native';
+import { useIntl } from '../providers/Intl';
 
 const TranslatedText = ({ id, ...props }: PropsType) => {
+	const { lang } = useIntl();
+
+	useEffect(() => {
+		// console.log('language: ', lang);
+	}, [lang]);
+
 	return <Text {...props}>{t(id)}</Text>;
 };
 
